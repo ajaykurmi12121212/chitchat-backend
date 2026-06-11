9e6b8481f6290649374dba21e25b23bf5454254b
ajaykurmi12121212 <141719545+ajaykurmi12121212@users.noreply.github.com>
2026-06-11 11:12:45 +0530

    fix: remove undefined phoneLogin route

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const { protect } = require('../middleware/auth')
 
 router.post('/send-otp',    c.sendOTP)
 router.post('/verify-otp',  c.verifyOTP)
-router.post('/phone-login', c.phoneLogin)  // purana bhi rakha backward compat ke liye
+
 router.get('/me',           protect, c.getMe)
 
 module.exports = router
